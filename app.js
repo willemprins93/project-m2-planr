aee6c0ff9f3d4cc5c30968dc61cedd77e5110ff2
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-08-25 09:52:12 +0200

    done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // Express View engine setup
 
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'public/views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
