3099ee42c8b7c0e1d7090fd578623eda75079ee9
Willem Prins <willemprins93@gmail.com>
2020-08-25 11:33:56 +0100

    create cities-list and cities-detail

--- routes/cities.routes.js
+++ routes/cities.routes.js
@@ -7,14 +7,24 @@ const City = require('../models/City.model');
 
 router.get('/', (req, res) => {
     City.find()
-      .then(citiesFromDB => {
-        console.log(citiesFromDB);
-        res.render('cities/cities-list',{ cities: citiesFromDB });
-      })
-      .catch(err =>
-        console.log(`Err while getting the cities from the  DB: ${err}`)
-      );
+        .then(citiesFromDB => {
+            console.log(citiesFromDB);
+            res.render('cities/cities-list',{ cities: citiesFromDB });
+        })
+        .catch(err =>
+            console.log(`Err while getting the cities from the  DB: ${err}`)
+        );
 });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    City.findById(id)
+        .then(cityFromDB => {
+            console.log('City found: ', cityFromDB)
+            res.render('cities/cities-detail', cityFromDB)
+        })
+        .catch(err => console.log('Error loading city: ', err))
+})
 
 
   
